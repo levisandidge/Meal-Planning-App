47f6a681757de5ee3b3dbd29cd4dcafe6e41708e
ClConr <clark.conrad@uky.edu>
2025-03-03 12:14:51 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `mealPlanner`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-sass"]
+};
