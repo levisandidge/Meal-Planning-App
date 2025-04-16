601cfe0b5380b97014dd24ffdc6cc77468050c69
BrianChu03 <bbc636@gmail.com>
2025-04-16 12:57:28 -0400

    Wrapped gatsby browser with user authentication Auth0,added domain and clientID into .env

---
+++ src/pages/meal-plans.js
@@ -0,0 +1,18 @@
+import * as React from "react"
+import { Container, Row, Col } from "react-bootstrap"
+
+import Layout from "../components/Layout"
+//import Calendar from "../components/calendar"; 
+
+export default function MealPage() {
+    return (
+      <>
+      <Layout>
+        <row>
+          <col>
+          </col>
+        </row>
+      </Layout>
+    </>
+  );
+};
