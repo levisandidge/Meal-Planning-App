d7c7dd9d3de2f3fef61d8c0a2f14d5ffa26ae885
BrianChu03 <bbc636@gmail.com>
2025-04-23 00:54:24 -0400

    change projectid and dataset to use .env.development file

--- Sanity/sanity.cli.js
+++ Sanity/sanity.cli.js
@@ -2,8 +2,8 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: '2ziz6el5',
-    dataset: 'production'
+    projectId: process.env.SANITY_PROJECT_ID,
+    dataset: process.env.SANITY_DATASET,
   },
   /**
    * Enable auto-updates for studios.
--- Sanity/sanity.config.js
+++ Sanity/sanity.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   name: 'default',
   title: 'MealPlanner',
 
-  projectId: '2ziz6el5',
-  dataset: 'production',
+  projectId: process.env.SANITY_PROJECT_ID,
+  dataset: process.env.SANITY_DATASET,
 
   plugins: [structureTool(), visionTool()],
 
